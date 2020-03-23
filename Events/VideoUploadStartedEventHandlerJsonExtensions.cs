4037f8a70425977ca921c7e53cd513b460370330
Vladyslav Zaiets <rutova2@gmail.com>
2020-03-23 21:18:40 +0200

    feat(events): json round-trip support for VideoUploadStartedEventHandler

---
+++ Events/VideoUploadStartedEventHandlerJsonExtensions.cs
@@ -0,0 +1,89 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace YouTubeShortsAutomator.Events;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extensions for VideoUploadStartedEventHandler
+/// </summary>
+public static class VideoUploadStartedEventHandlerJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
+        PropertyNameCaseInsensitive = true,
+    };
+
+    /// <summary>
+    /// Serializes the VideoUploadStartedEventHandler instance to a JSON string
+    /// </summary>
+    /// <param name="value">The event handler instance to serialize</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability</param>
+    /// <returns>A JSON string representation of the event handler</returns>
+    /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+    public static string ToJson(this VideoUploadStartedEventHandler value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a VideoUploadStartedEventHandler instance
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <returns>A VideoUploadStartedEventHandler instance, or null if JSON is empty or whitespace</returns>
+    /// <exception cref="ArgumentNullException">Thrown when json is null</exception>
+    /// <exception cref="JsonException">Thrown when JSON is invalid or cannot be deserialized</exception>
+    public static VideoUploadStartedEventHandler? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<VideoUploadStartedEventHandler>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a VideoUploadStartedEventHandler instance
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <param name="value">Receives the deserialized instance if successful, otherwise null</param>
+    /// <returns>True if deserialization succeeded; otherwise, false</returns>
+    /// <exception cref="ArgumentNullException">Thrown when json is null</exception>
+    public static bool TryFromJson(string json, out VideoUploadStartedEventHandler? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return false;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<VideoUploadStartedEventHandler>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
