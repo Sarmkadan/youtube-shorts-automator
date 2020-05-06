6d8deda087bb63cfc3bb9dfd2e8b2eec21364985
Vladyslav Zaiets <rutova2@gmail.com>
2020-05-06 20:52:55 +0300

    handy extensions for Analytics
    will circle back to tests later.

--- src/Services/AnalyticsServiceExtensions.cs
+++ src/Services/AnalyticsServiceExtensions.cs
@@ -3,29 +3,37 @@
 // CTO & Software Architect
 // =====================================================================
 
+using System;
 using YouTubeShortAutomator.Domain.Models;
-using Microsoft.Extensions.Logging;
 
 namespace YouTubeShortAutomator.Services;
 
 public static class AnalyticsServiceExtensions
 {
     /// <summary>
-    /// Calculates the engagement ratio (views to likes ratio) for a video
+    /// Calculates the engagement ratio (likes to views ratio) for a video
     /// </summary>
     /// <param name="service">The analytics service instance</param>
     /// <param name="videoShortId">The video short ID</param>
-    /// <returns>Engagement ratio as a decimal percentage, or 0 if no likes</returns>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>Engagement ratio as a decimal percentage, or 0 if no views or likes</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="service"/> is null</exception>
     public static async Task<decimal> CalculateEngagementRatioAsync(this AnalyticsService service, int videoShortId, CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(service);
+
         var analytics = await service.GetVideoAnalyticsAsync(videoShortId, cancellationToken);
 
-        if (analytics == null || analytics.LikeCount == 0)
+        if (analytics == null || analytics.ViewCount == 0)
         {
             return 0;
         }
 
-        // Engagement ratio = (likes / views) * 100
+        if (analytics.LikeCount == 0)
+        {
+            return 0;
+        }
+
         return (decimal)analytics.LikeCount / analytics.ViewCount * 100;
     }
 
@@ -35,12 +43,24 @@ public static class AnalyticsServiceExtensions
     /// <param name="service">The analytics service instance</param>
     /// <param name="limit">Number of videos to return</param>
     /// <param name="sortBy">Metric to sort by (views, likes, comments, shares, engagement)</param>
+    /// <param name="cancellationToken">Cancellation token</param>
     /// <returns>Top performing videos sorted by the specified metric</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="service"/> is null</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="sortBy"/> is null</exception>
+    /// <exception cref="ArgumentException"><paramref name="sortBy"/> is empty or whitespace</exception>
     public static async Task<IEnumerable<AnalyticsData>> GetTopPerformingVideosAsync(this AnalyticsService service, int limit, string sortBy, CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(service);
+        ArgumentNullException.ThrowIfNull(sortBy);
+
+        if (string.IsNullOrWhiteSpace(sortBy))
+        {
+            throw new ArgumentException("Sort metric cannot be empty or whitespace.", nameof(sortBy));
+        }
+
         var topVideos = await service.GetTopPerformingVideosAsync(limit, cancellationToken);
 
-        return sortBy.ToLower() switch
+        return sortBy.ToLowerInvariant() switch
         {
             "views" => topVideos.OrderByDescending(v => v.ViewCount),
             "likes" => topVideos.OrderByDescending(v => v.LikeCount),
@@ -55,9 +75,13 @@ public static class AnalyticsServiceExtensions
     /// Calculates the average view duration across all videos in hours
     /// </summary>
     /// <param name="service">The analytics service instance</param>
+    /// <param name="cancellationToken">Cancellation token</param>
     /// <returns>Average view duration in hours</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="service"/> is null</exception>
     public static async Task<double> CalculateAverageViewDurationHoursAsync(this AnalyticsService service, CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(service);
+
         var allAnalytics = await service.GetTopPerformingVideosAsync(int.MaxValue, cancellationToken);
 
         if (!allAnalytics.Any())
@@ -75,9 +99,13 @@ public static class AnalyticsServiceExtensions
     /// <param name="service">The analytics service instance</param>
     /// <param name="videoShortId1">First video ID</param>
     /// <param name="videoShortId2">Second video ID</param>
+    /// <param name="cancellationToken">Cancellation token</param>
     /// <returns>Comparison string with performance metrics</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="service"/> is null</exception>
     public static async Task<string> CompareVideoPerformanceAsync(this AnalyticsService service, int videoShortId1, int videoShortId2, CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(service);
+
         var video1 = await service.GetVideoAnalyticsAsync(videoShortId1, cancellationToken);
         var video2 = await service.GetVideoAnalyticsAsync(videoShortId2, cancellationToken);
 
@@ -98,7 +126,7 @@ public static class AnalyticsServiceExtensions
 
         var comparison = $"Video Performance Comparison:\n";
         comparison += $"Video {videoShortId1}: {video1.ViewCount:N0} views, {video1.LikeCount:N0} likes, {video1.EngagementRate:F2}% engagement\n";
-        comparison += $"Video {videoShortId2}: {video2.ViewCount:N0} views, {video2.LikeCount:N0} likes, {video2.EngagementRate:F2}% engagement\n\n";
+        comparison += $"Video {videoShortId2}: {video2.ViewCount:N0} views, {video2.LikeCount:N0} likes, {video2.EngagementRate:F2}% engagement\n";
 
         var viewsDiff = video1.ViewCount - video2.ViewCount;
         var likesDiff = video1.LikeCount - video2.LikeCount;
