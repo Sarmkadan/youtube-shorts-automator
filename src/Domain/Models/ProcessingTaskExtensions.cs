473ded6360c25fd54432aa2e2b20bd055c526d5f
Vladyslav Zaiets <rutova2@gmail.com>
2020-05-06 13:09:49 +0300

    handy extensions for ProcessingTask
    one-liner really but wanted a clean commit.

--- src/Domain/Models/ProcessingTaskExtensions.cs
+++ src/Domain/Models/ProcessingTaskExtensions.cs
@@ -1,80 +1,107 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 using YouTubeShortAutomator.Constants;
-using YouTubeShortAutomator.Domain.Models;
 
 namespace YouTubeShortAutomator.Domain.Models;
 
+/// <summary>
+/// Provides extension methods for <see cref="ProcessingTask"/> to simplify common task operations and calculations.
+/// </summary>
 public static class ProcessingTaskExtensions
 {
-    /// <summary>
-    /// Calculates the estimated completion percentage based on task status and elapsed time.
-    /// Returns 0 for pending/queued tasks, 50 for processing tasks, and 100 for completed/failed/uploaded tasks.
-    /// </summary>
-    public static int GetCompletionPercentage(this ProcessingTask task)
-    {
-        return task.Status switch
-        {
-            ProcessingStatus.Completed or ProcessingStatus.Failed or ProcessingStatus.Uploaded => 100,
-            ProcessingStatus.Processing => 50,
-            ProcessingStatus.Pending or ProcessingStatus.Queued or ProcessingStatus.OnHold => 0,
-            ProcessingStatus.Cancelled => 0,
-            _ => 0
-        };
-    }
+	/// <summary>
+	/// Calculates the estimated completion percentage based on task status.
+	/// Returns 100 for terminal states (Completed, Failed, Uploaded), 50 for active processing,
+	/// and 0 for pending/queued/on-hold states.
+	/// </summary>
+	/// <param name="task">The processing task.</param>
+	/// <exception cref="ArgumentNullException"><paramref name="task"/> is <see langword="null"/>.</exception>
+	public static int GetCompletionPercentage(this ProcessingTask task)
+	{
+		ArgumentNullException.ThrowIfNull(task);
 
-    /// <summary>
-    /// Determines if the task is currently in a processing state (Processing, Queued, or OnHold).
-    /// </summary>
-    public static bool IsProcessing(this ProcessingTask task)
-    {
-        return task.Status is ProcessingStatus.Processing or ProcessingStatus.Queued or ProcessingStatus.OnHold;
-    }
+		return task.Status switch
+		{
+			ProcessingStatus.Completed or ProcessingStatus.Failed or ProcessingStatus.Uploaded => 100,
+			ProcessingStatus.Processing => 50,
+			ProcessingStatus.Pending or ProcessingStatus.Queued or ProcessingStatus.OnHold => 0,
+			ProcessingStatus.Cancelled => 0,
+			_ => 0
+		};
+	}
 
-    /// <summary>
-    /// Determines if the task is in a terminal state (Completed, Failed, Uploaded, or Cancelled).
-    /// </summary>
-    public static bool IsTerminal(this ProcessingTask task)
-    {
-        return task.Status is ProcessingStatus.Completed or ProcessingStatus.Failed or ProcessingStatus.Uploaded or ProcessingStatus.Cancelled;
-    }
+	/// <summary>
+	/// Determines if the task is currently in an active processing state (Processing, Queued, or OnHold).
+	/// </summary>
+	/// <param name="task">The processing task.</param>
+	/// <exception cref="ArgumentNullException"><paramref name="task"/> is <see langword="null"/>.</exception>
+	public static bool IsProcessing(this ProcessingTask task)
+	{
+		ArgumentNullException.ThrowIfNull(task);
 
-    /// <summary>
-    /// Gets the duration of the task in a human-readable format.
-    /// Returns "Not started" for tasks without StartedAt, and formatted time for completed tasks.
-    /// </summary>
-    public static string GetDurationDisplay(this ProcessingTask task)
-    {
-        if (task.StartedAt == default)
-        {
-            return "Not started";
-        }
+		return task.Status is ProcessingStatus.Processing or ProcessingStatus.Queued or ProcessingStatus.OnHold;
+	}
 
-        if (task.CompletedAt.HasValue && task.ElapsedTime.HasValue)
-        {
-            return $"{task.ElapsedTime.Value.TotalSeconds:F2}s";
-        }
+	/// <summary>
+	/// Determines if the task is in a terminal state (Completed, Failed, Uploaded, or Cancelled).
+	/// </summary>
+	/// <param name="task">The processing task.</param>
+	/// <exception cref="ArgumentNullException"><paramref name="task"/> is <see langword="null"/>.</exception>
+	public static bool IsTerminal(this ProcessingTask task)
+	{
+		ArgumentNullException.ThrowIfNull(task);
 
-        if (task.Status == ProcessingStatus.Processing)
-        {
-            var currentDuration = DateTime.UtcNow - task.StartedAt;
-            return $"{currentDuration.TotalSeconds:F2}s (in progress)";
-        }
+		return task.Status is ProcessingStatus.Completed or ProcessingStatus.Failed or ProcessingStatus.Uploaded or ProcessingStatus.Cancelled;
+	}
 
-        return "N/A";
-    }
+	/// <summary>
+	/// Gets the duration of the task in a human-readable format.
+	/// Returns "Not started" for tasks without StartedAt, formatted duration for completed tasks,
+	/// current duration with "(in progress)" suffix for active processing tasks, and "N/A" for other states.
+	/// </summary>
+	/// <param name="task">The processing task.</param>
+	/// <exception cref="ArgumentNullException"><paramref name="task"/> is <see langword="null"/>.</exception>
+	public static string GetDurationDisplay(this ProcessingTask task)
+	{
+		ArgumentNullException.ThrowIfNull(task);
 
-    /// <summary>
-    /// Updates the task priority by a relative delta.
-    /// </summary>
-    /// <param name="task">The processing task</param>
-    /// <param name="delta">The priority change amount (positive or negative)</param>
-    public static void AdjustPriority(this ProcessingTask task, int delta)
-    {
-        task.Priority = Math.Max(1, Math.Min(10, task.Priority + delta));
-        task.UpdatedAt = DateTime.UtcNow;
-    }
+		if (task.StartedAt == default)
+		{
+			return "Not started";
+		}
+
+		if (task.CompletedAt.HasValue)
+		{
+			// Calculate duration from timestamps when CompletedAt is available
+			var duration = task.CompletedAt.Value - task.StartedAt;
+			return $"{duration.TotalSeconds:F2}s";
+		}
+
+		if (task.Status == ProcessingStatus.Processing)
+		{
+			// Calculate current duration for in-progress tasks
+			var currentDuration = DateTime.UtcNow - task.StartedAt;
+			return $"{currentDuration.TotalSeconds:F2}s (in progress)";
+		}
+
+		return "N/A";
+	}
+
+	/// <summary>
+	/// Updates the task priority by a relative delta, ensuring it stays within the valid range [1, 10].
+	/// Also updates the UpdatedAt timestamp to mark when the priority was last modified.
+	/// </summary>
+	/// <param name="task">The processing task.</param>
+	/// <param name="delta">The priority change amount (positive or negative).</param>
+	/// <exception cref="ArgumentNullException"><paramref name="task"/> is <see langword="null"/>.</exception>
+	public static void AdjustPriority(this ProcessingTask task, int delta)
+	{
+		ArgumentNullException.ThrowIfNull(task);
+
+		task.Priority = Math.Max(1, Math.Min(10, task.Priority + delta));
+		task.UpdatedAt = DateTime.UtcNow;
+	}
 }
