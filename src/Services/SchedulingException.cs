a3c9ee54cbd945c16c4d1533880f027d869bd480
Vladyslav Zaiets <rutova2@gmail.com>
2020-05-14 09:07:51 +0300

    refactor(services): tighten guard clauses in SchedulingException

--- src/Services/SchedulingException.cs
+++ src/Services/SchedulingException.cs
@@ -5,16 +5,41 @@
 
 namespace YouTubeShortAutomator.Services;
 
+/// <summary>
+/// Represents an exception that occurs during scheduling.
+/// </summary>
 public class SchedulingException : Exception
 {
+    /// <summary>
+    /// Gets the scheduled time of the job that failed.
+    /// </summary>
     public DateTime? ScheduledTime { get; set; }
+
+    /// <summary>
+    /// Gets the ID of the job that failed.
+    /// </summary>
     public int? JobId { get; set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SchedulingException"/> class.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public SchedulingException(string message) : base(message) { }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SchedulingException"/> class.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">The inner exception that caused this exception.</param>
     public SchedulingException(string message, Exception innerException)
         : base(message, innerException) { }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SchedulingException"/> class.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="jobId">The ID of the job that failed.</param>
+    /// <param name="scheduledTime">The scheduled time of the job that failed.</param>
     public SchedulingException(string message, int jobId, DateTime scheduledTime)
         : base(message)
     {
