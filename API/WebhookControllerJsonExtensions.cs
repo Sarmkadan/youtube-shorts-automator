f2598b76f2ea652894f73445d28bb9d5af549d6b
Vladyslav Zaiets <rutova2@gmail.com>
2020-06-08 15:57:22 +0300

    serialize WebhookControllerJson properly
    should be backwards compatible.

---
+++ API/WebhookControllerJsonExtensions.cs
@@ -0,0 +1,84 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace YouTubeShortsAutomator.API;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for WebhookController
+/// </summary>
+public static class WebhookControllerJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+    };
+
+    /// <summary>
+    /// Serializes a WebhookController instance to JSON string
+    /// </summary>
+    /// <param name="value">The WebhookController instance to serialize</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability</param>
+    /// <returns>A JSON string representation of the WebhookController</returns>
+    /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+    public static string ToJson(this WebhookController value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a WebhookController instance
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <returns>A WebhookController instance, or null if deserialization fails</returns>
+    /// <exception cref="ArgumentException">Thrown when json is null or empty</exception>
+    public static WebhookController? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            return JsonSerializer.Deserialize<WebhookController>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a WebhookController instance
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <param name="value">Receives the deserialized WebhookController instance if successful</param>
+    /// <returns>True if deserialization succeeds; otherwise, false</returns>
+    /// <exception cref="ArgumentException">Thrown when json is null or empty</exception>
+    public static bool TryFromJson(string json, out WebhookController? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<WebhookController>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
