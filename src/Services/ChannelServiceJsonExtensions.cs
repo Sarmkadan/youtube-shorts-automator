ea8e56d2422ebc112b4fea8471ba4a792499af3e
Vladyslav Zaiets <rutova2@gmail.com>
2020-06-25 16:41:29 +0300

    serialize ChannelServiceJson properly
    should be backwards compatible.

---
+++ src/Services/ChannelServiceJsonExtensions.cs
@@ -0,0 +1,88 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// ===================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace YouTubeShortAutomator.Services;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="ChannelService"/>.
+/// </summary>
+public static class ChannelServiceJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="ChannelService"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The channel service instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the channel service.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this ChannelService value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="ChannelService"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized <see cref="ChannelService"/> instance, or null if the JSON is empty.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static ChannelService? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<ChannelService>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="ChannelService"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized <see cref="ChannelService"/> instance if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    public static bool TryFromJson(string json, out ChannelService? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        value = null;
+
+        try
+        {
+            if (!string.IsNullOrWhiteSpace(json))
+            {
+                value = JsonSerializer.Deserialize<ChannelService>(json, _jsonOptions);
+                return true;
+            }
+
+            return false;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
