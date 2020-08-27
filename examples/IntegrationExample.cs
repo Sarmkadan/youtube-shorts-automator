46031ed3c4600d6b55233f44c262b3e1650ad210
Vladyslav Zaiets <rutova2@gmail.com>
2020-08-27 16:39:51 +0300

    refactor: tighten guard clauses in IntegrationExample

--- examples/IntegrationExample.cs
+++ examples/IntegrationExample.cs
@@ -9,9 +9,15 @@ using YouTubeShortsAutomator.Application.Services;
 using YouTubeShortsAutomator.Extensions;
 using YouTubeShortsAutomator.Infrastructure.Extensions;
 
-// Integration example: Wiring up into ASP.NET Core DI
+/// <summary>
+/// Provides an example of how to integrate the YouTube Shorts Automator into an ASP.NET Core application.
+/// </summary>
 public class IntegrationExample
 {
+    /// <summary>
+    /// Configures the services for the application.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> instance.</param>
     public static void ConfigureServices(IServiceCollection services)
     {
         Console.WriteLine("=== Integration: Registering Services ===");
@@ -27,15 +33,24 @@ public class IntegrationExample
         services.AddHostedService<Worker>();
     }
 
+    /// <summary>
+    /// A sample worker that demonstrates how to use the <see cref="IVideoProcessingService"/> instance.
+    /// </summary>
     public class Worker : BackgroundService
     {
-        private readonly IVideoProcessingService _processingService;
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Worker"/> class.
+        /// </summary>
+        /// <param name="processingService">The <see cref="IVideoProcessingService"/> instance.</param>
         public Worker(IVideoProcessingService processingService)
         {
             _processingService = processingService;
         }
 
+        /// <summary>
+        /// Executes the worker's background task.
+        /// </summary>
+        /// <param name="stoppingToken">The <see cref="CancellationToken"/> instance.</param>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             Console.WriteLine("Worker started...");
