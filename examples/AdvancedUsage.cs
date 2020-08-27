a4d2930c90b4c6636c2ed0752a785ee71264a2e7
Vladyslav Zaiets <rutova2@gmail.com>
2020-08-27 16:07:58 +0300

    tighten guard clauses in AdvancedUsage

--- examples/AdvancedUsage.cs
+++ examples/AdvancedUsage.cs
@@ -9,19 +9,34 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 
-// Advanced usage: configuration, options, and error handling
+/// <summary>
+/// Demonstrates advanced usage of the YouTube Shorts Automator API.
+/// </summary>
 public class AdvancedUsage
 {
+    /// <summary>
+    /// The base URL of the API.
+    /// </summary>
     private const string BASE_URL = "http://localhost:5000/api";
-    private const string API_KEY = "your-api-key-here";
-    private readonly HttpClient _client;
 
+    /// <summary>
+    /// The API key used for authentication.
+    /// </summary>
+    private const string API_KEY = "your-api-key-here";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AdvancedUsage"/> class.
+    /// </summary>
     public AdvancedUsage()
     {
         _client = new HttpClient();
         _client.DefaultRequestHeaders.Add("X-API-Key", API_KEY);
     }
 
+    /// <summary>
+    /// Runs the advanced usage example.
+    /// </summary>
+    /// <returns>A task representing the asynchronous operation.</returns>
     public async Task RunAsync()
     {
         Console.WriteLine("=== Advanced Usage: Scheduled Upload with Config ===");
@@ -31,11 +46,29 @@ public class AdvancedUsage
             // Custom scheduling options
             var jobRequest = new
             {
+                /// <summary>
+                /// The ID of the video short to upload.
+                /// </summary>
                 VideoShortId = 1,
+                /// <summary>
+                /// The title of the video.
+                /// </summary>
                 Title = "Advanced Automation",
+                /// <summary>
+                /// The description of the video.
+                /// </summary>
                 Description = "This is an automated upload with custom config.",
+                /// <summary>
+                /// The tags associated with the video.
+                /// </summary>
                 Tags = new List<string> { "dotnet", "shorts", "automation" },
+                /// <summary>
+                /// The scheduled upload time.
+                /// </summary>
                 ScheduledUploadTime = DateTime.UtcNow.AddHours(24),
+                /// <summary>
+                /// The retry strategy to use.
+                /// </summary>
                 RetryStrategy = "ExponentialBackoff"
             };
 
@@ -55,14 +88,29 @@ public class AdvancedUsage
         }
         catch (HttpRequestException ex)
         {
+            /// <summary>
+            /// The network error that occurred.
+            /// </summary>
             Console.WriteLine($"✗ Network error: {ex.Message}");
         }
         catch (Exception ex)
         {
+            /// <summary>
+            /// The unexpected error that occurred.
+            /// </summary>
             Console.WriteLine($"✗ Unexpected error: {ex.Message}");
         }
     }
 
+    /// <summary>
+    /// The HTTP client used for API requests.
+    /// </summary>
+    private readonly HttpClient _client;
+
+    /// <summary>
+    /// The static main entry point of the application.
+    /// </summary>
+    /// <param name="args">The command-line arguments.</param>
     static async Task Main(string[] args)
     {
         var example = new AdvancedUsage();
