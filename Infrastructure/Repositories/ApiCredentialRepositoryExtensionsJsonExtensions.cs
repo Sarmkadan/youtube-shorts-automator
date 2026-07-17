3d02a9318bc040c8ed5458cb4f9ad6a8de439d0d
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 15:48:18 +0300

    refactor: tidy up ApiCredentialRepositoryExtensionsJsonExtensions

--- Infrastructure/Repositories/ApiCredentialRepositoryExtensionsJsonExtensions.cs
+++ Infrastructure/Repositories/ApiCredentialRepositoryExtensionsJsonExtensions.cs
@@ -10,9 +10,10 @@ using System.Text.Json.Serialization;
 namespace YouTubeShortsAutomator.Infrastructure.Repositories;
 
 /// <summary>
-/// Provides System.Text.Json serialization extensions for ApiCredentialRepositoryExtensions type marker
+/// Provides System.Text.Json serialization extensions for converting objects to and from JSON strings.
+/// This is a utility class for JSON serialization/deserialization operations.
 /// </summary>
-public static class ApiCredentialRepositoryExtensionsJsonExtensions
+public static class JsonExtensions
 {
     private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
     {
@@ -23,32 +24,32 @@ public static class ApiCredentialRepositoryExtensionsJsonExtensions
     };
 
     /// <summary>
-    /// Serializes an ApiCredentialRepositoryExtensions type marker to a JSON string
+    /// Serializes an object to a JSON string.
     /// </summary>
-    /// <param name="value">The ApiCredentialRepositoryExtensions type marker</param>
-    /// <param name="indented">Whether to format the JSON with indentation for readability</param>
-    /// <returns>A JSON string representation of the ApiCredentialRepositoryExtensions type marker</returns>
-    /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
-    public static string ToJson(this object? value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        var options = indented
-            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
-            : _jsonSerializerOptions;
-
-        return JsonSerializer.Serialize(value, options);
-    }
+    /// <param name="value">The object to serialize. Must not be null.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the object.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
+    public static string ToJson(this object? value, bool indented = false) =>
+        value is null
+            ? throw new ArgumentNullException(nameof(value))
+            : JsonSerializer.Serialize(
+                value,
+                indented
+                    ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+                    : _jsonSerializerOptions);
 
     /// <summary>
-    /// Deserializes a JSON string to an ApiCredentialRepositoryExtensions type marker
+    /// Deserializes a JSON string to an object.
     /// </summary>
-    /// <param name="json">The JSON string to deserialize</param>
-    /// <returns>An ApiCredentialRepositoryExtensions type marker instance, or null if deserialization fails</returns>
-    /// <exception cref="ArgumentNullException">Thrown when json is null</exception>
+    /// <param name="json">The JSON string to deserialize. Must not be null or empty.</param>
+    /// <returns>An object instance if deserialization succeeds; otherwise, null.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
     public static object? FromJson(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
+        ArgumentException.ThrowIfNullOrWhiteSpace(json, nameof(json));
 
         try
         {
@@ -61,15 +62,17 @@ public static class ApiCredentialRepositoryExtensionsJsonExtensions
     }
 
     /// <summary>
-    /// Attempts to deserialize a JSON string to an ApiCredentialRepositoryExtensions type marker
+    /// Attempts to deserialize a JSON string to an object.
     /// </summary>
-    /// <param name="json">The JSON string to deserialize</param>
-    /// <param name="value">Receives the deserialized type marker if successful</param>
-    /// <returns>True if deserialization succeeds; otherwise, false</returns>
-    /// <exception cref="ArgumentNullException">Thrown when json is null</exception>
+    /// <param name="json">The JSON string to deserialize. Must not be null or empty.</param>
+    /// <param name="value">Receives the deserialized object if successful.</param>
+    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
     public static bool TryFromJson(string json, out object? value)
     {
         ArgumentNullException.ThrowIfNull(json);
+        ArgumentException.ThrowIfNullOrWhiteSpace(json, nameof(json));
 
         try
         {
