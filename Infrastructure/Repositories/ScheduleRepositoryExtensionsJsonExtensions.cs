908158a3637167eca3e762bd31949ee4571041a0
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 13:33:51 +0300

    Polish ScheduleRepositoryExtensionsJsonExtensions
    found this while looking at something else.

--- Infrastructure/Repositories/ScheduleRepositoryExtensionsJsonExtensions.cs
+++ Infrastructure/Repositories/ScheduleRepositoryExtensionsJsonExtensions.cs
@@ -4,7 +4,7 @@ using System.Text.Json;
 namespace YouTubeShortsAutomator.Infrastructure.Repositories
 {
     /// <summary>
-    /// Provides System.Text.Json serialization extensions for ScheduleRepositoryExtensions type marker.
+    /// Provides System.Text.Json serialization extensions for converting objects to and from JSON strings.
     /// </summary>
     public static class ScheduleRepositoryExtensionsJsonExtensions
     {
@@ -15,35 +15,25 @@ namespace YouTubeShortsAutomator.Infrastructure.Repositories
         };
 
         /// <summary>
-        /// Serializes a ScheduleRepositoryExtensions type marker to a JSON string.
+        /// Serializes an object to a JSON string.
         /// </summary>
-        /// <param name="value">The ScheduleRepositoryExtensions type marker.</param>
+        /// <param name="value">The object to serialize.</param>
         /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-        /// <returns>A JSON string representation of the ScheduleRepositoryExtensions type marker.</returns>
+        /// <returns>A JSON string representation of the object.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/></exception>
         public static string ToJson(this object? value, bool indented = false)
-        {
-            ArgumentNullException.ThrowIfNull(value);
-
-            var options = indented
-                ? new JsonSerializerOptions(_jsonSerializerOptions)
-                {
-                    WriteIndented = true
-                }
-                : _jsonSerializerOptions;
-
-            return JsonSerializer.Serialize(value, options);
-        }
+            => JsonSerializer.Serialize(value, indented ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true } : _jsonSerializerOptions);
 
         /// <summary>
-        /// Deserializes a JSON string to a ScheduleRepositoryExtensions type marker.
+        /// Deserializes a JSON string to an object.
         /// </summary>
         /// <param name="json">The JSON string to deserialize.</param>
-        /// <returns>A ScheduleRepositoryExtensions type marker instance, or <see langword="null"/> if deserialization fails.</returns>
+        /// <returns>An object instance, or <see langword="null"/> if deserialization fails.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/></exception>
         public static object? FromJson(string json)
         {
             ArgumentNullException.ThrowIfNull(json);
+            ArgumentException.ThrowIfNullOrWhiteSpace(json);
 
             try
             {
@@ -56,15 +46,16 @@ namespace YouTubeShortsAutomator.Infrastructure.Repositories
         }
 
         /// <summary>
-        /// Attempts to deserialize a JSON string to a ScheduleRepositoryExtensions type marker.
+        /// Attempts to deserialize a JSON string to an object.
         /// </summary>
         /// <param name="json">The JSON string to deserialize.</param>
-        /// <param name="value">The deserialized ScheduleRepositoryExtensions type marker, or <see langword="null"/> if deserialization fails.</param>
+        /// <param name="value">The deserialized object, or <see langword="null"/> if deserialization fails.</param>
         /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/></exception>
         public static bool TryFromJson(string json, out object? value)
         {
             ArgumentNullException.ThrowIfNull(json);
+            ArgumentException.ThrowIfNullOrWhiteSpace(json);
 
             try
             {
